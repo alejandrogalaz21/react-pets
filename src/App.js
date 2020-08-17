59b5474fbbf2e4b2ae677224e382ba3095332dc5
alex <jesus.galaz@softtek.com>
2020-08-17 12:08:26 -0500

    create form component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import form from './components/Form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <div className='container'>
+        <div className='one-half column'>1</div>
+        <div className='one-half column'>2</div>
+      </div>
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
